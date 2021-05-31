b1170f664fc9d98748e32bc2ae9f7eb09ab54299
Faskomilo <oscarve.do@outlook.com>
2021-05-30 23:58:41 -0500

    added login for profiles

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -11,5 +11,6 @@ router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('text_view/', views.TextAPIView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
